37adac59db1616189f078824648342b15b467387
Hari <Hari.Prasath@bt.com>
2018-06-26 14:12:52 +0100

    Initial Commit

---
+++ tryNewIntegration1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tryNewIntegration1
+//
+//  Created by Hari Prasath on 26/06/2018.
+//  Copyright © 2018 Hari Prasath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
